8e7ac8c1d80787e6126b30972caea8a2f97cc530
Simeon Mugisha <1554739+cmeon@users.noreply.github.com>
2020-10-21 14:20:09 +0000

    Add interractivity

--- src/main/resources/seed_reports.sh
+++ src/main/resources/seed_reports.sh
@@ -25,6 +25,13 @@ HERE=`cd \`dirname $0\`; pwd`
 REPORTS=$HERE/reports
 SYSTEM=$HERE/system
 
+# Text formatting
+bold=$(tput bold)
+normal=$(tput sgr0)
+underline=$(tput smul)
+end_underline=$(tput rmul)
+red=$(tput setaf 1)
+
 function install_ddl() {
 	local ddl=$1
 
@@ -65,12 +72,71 @@ function install_ddl() {
 		else
 			break
 		fi
+		echo -e "\033[0m"
 	done
-	echo -e "\033[0m"
 }
 
-# Install the DDL - the calendar table needs to be first
-install_ddl $REPORTS/calendar.sql
-for r in `find $REPORTS -type f -name '*.sql' -o -name '*.ddl' -maxdepth 1`; do install_ddl $r; done
-for r in `find $SYSTEM -type f -name '*.sql' -o -name '*.ddl' -maxdepth 1`; do install_ddl $r; done
+
+function seed()
+{
+	local folder=$1
+
+	echo "Seeding folder $folder"
+	for r in `find $folder -type f -name '*.sql' -o -name '*.ddl' -maxdepth 1`; do install_ddl $r; done
+}
+
+function print_help
+{
+	script_name=`basename "$0"`
+
+	echo "${underline}$script_name${normal} generates java or typescript clients with ease"
+	echo
+	echo "${bold}Usage:${normal}   $script_name [OPTION]"
+	echo
+	echo "${bold}Example:${normal} client_generate -l java -v 0.0.3 -h http://localhost:8067 -n lpco"
+	echo
+	echo "${bold}Options:${normal}"
+	echo "  -h --help                         print this help"
+	echo "  -l|--language [language]          set language"
+	echo "  -n|--service-name [service_name]  set service name, eg. lpco"
+	echo "  -H|--host [hosturl]               set service host, eg. http://localhost:8067"
+	echo "  -v|--version [client version]     set client version, default: 0.0.1-SNAPSHOT for java and 0.0.1 for typescript"
+	echo
+	echo "${bold}Language Options:${normal}"
+	echo "   1. ${bold}Java client:${normal}       [java|java-resttemplate|java-gson-okhttp]"
+	echo
+	echo "   2. ${bold}Angular TS client:${normal} [js|ts|angular|typescript]"
+	echo "      --ng-version   set angular version"
+	echo
+}
+
+
+while [ ! -z "$1" ]
+do
+	case "$1" in
+		"-h"|"--help")
+			print_help
+			exit
+			;;
+		"-f"|"--folder")
+			shift
+			if [ ! -d $1 ]; then
+				log_error "Invalid folder" "Should be a directory"
+			fi
+			seed $HERE/$1
+			;;
+		"-d"|"--default")
+			shift
+			install_ddl $REPORTS/calendar.sql
+			seed $REPORTS
+			seed $SYSTEM
+			;;
+		*)
+			log_error "Invalid option" "$1 $2$"
+			echo
+			print_help
+			exit 3
+	esac
+	shift
+done
 
